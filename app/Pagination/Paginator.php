2246aaf39a29f7f04decb610a5896324cf47dfe4
AlexeiZhurov <zhurov@greensight.ru>
2023-01-15 13:20:48 +0300

    fix: рефакторил

--- app/Pagination/Paginator.php
+++ app/Pagination/Paginator.php
@@ -3,42 +3,42 @@
 namespace App\Pagination;
 
 use Illuminate\Database\Eloquent\Builder;
+use \Illuminate\Support\Collection;
 
 class Paginator
 {
 
-    protected $collection;
 
-    public static function create(Builder $queryBuilder, int $limit = 25)
-    {
-        return (new self())->build($queryBuilder, $limit);
-    }
-
-    public function build(Builder $queryBuilder, int $limit)
+    public static function build(Builder $queryBuilder, int $limit): Collection
     {
         $LengthAwarePaginator = $queryBuilder->paginate($limit);
-        $buffer = [];
 
-        $buffer['data'] = $LengthAwarePaginator->getCollection();
-        $buffer['pagination']['total'] = $LengthAwarePaginator->total();
-        $buffer['pagination']['per_page'] = $LengthAwarePaginator->perPage();
-        $buffer['pagination']['current_page'] = $LengthAwarePaginator->currentPage();
-        $buffer['pagination']['count_item_current_page'] = $LengthAwarePaginator->count();
-        $buffer['pagination']['next_page_url'] = $LengthAwarePaginator->nextPageUrl();
-        $buffer['pagination']['prev_page_url'] = $LengthAwarePaginator->previousPageUrl();
-        $buffer['pagination']['last_page'] = $LengthAwarePaginator->lastPage();
+        $data = $LengthAwarePaginator->getCollection();
+        $pagination = [];
+
+        $pagination['total'] = $LengthAwarePaginator->total();
+        $pagination['per_page'] = $LengthAwarePaginator->perPage();
+        $pagination['current_page'] = $LengthAwarePaginator->currentPage();
+        $pagination['count_item_current_page'] = $LengthAwarePaginator->count();
+        $pagination['next_page_url'] = $LengthAwarePaginator->nextPageUrl();
+        $pagination['prev_page_url'] = $LengthAwarePaginator->previousPageUrl();
+        $pagination['last_page'] = $LengthAwarePaginator->lastPage();
+
         if ($LengthAwarePaginator->nextPageUrl() != null) {
-            $buffer['pagination']['next_page'] = $LengthAwarePaginator->currentPage() + 1;
+            $pagination['next_page'] = $LengthAwarePaginator->currentPage() + 1;
         } else {
-            $buffer['pagination']['next_page'] = null;
+            $pagination['next_page'] = null;
         }
-        if($LengthAwarePaginator->currentPage() != 1){
-            $buffer['pagination']['prev_page'] = $LengthAwarePaginator->currentPage() - 1;
-        }else{
-            $buffer['pagination']['prev_page'] = null;
+        if ($LengthAwarePaginator->currentPage() != 1) {
+            $pagination['prev_page'] = $LengthAwarePaginator->currentPage() - 1;
+        } else {
+            $pagination['prev_page'] = null;
         }
-        $this->collection = collect($buffer);
-        return $this->collection;
+        
+        $result = collect(
+            ['data' => $data, 'pagination' => $pagination]
+        );
+        return $result;
     }
 
 }
--- app/Queries/Card/getCardsUserQuery.php
+++ app/Queries/Card/getCardsUserQuery.php
@@ -2,24 +2,27 @@
 namespace App\Queries\Card;
 
 use App\Models\Card;
-use Auth;
 use App\Pagination\Paginator;
+use Auth;
 use Illuminate\Support\Collection;
+
 class getCardsUserQuery
 {
 
-    public static function find() : Collection
+    public static function find(): Collection
     {
         $userId = Auth::id();
-        $cardsUserQuery = 
+        $cardsUserQuery =
         Card::query()
-        ->where('user_id', $userId)
-        ->RightJoin('words', 'words.id', '=', 'cards.word_id')
-        ->select('cards.id','cards.level','cards.url','cards.user_id','cards.created_at','cards.repeats','words.value','words.data');
-        $cardsUser = Paginator::create($cardsUserQuery,10);
+            ->where('user_id', $userId)
+            ->RightJoin('words', 'words.id', '=', 'cards.word_id')
+            ->select(
+                'cards.id', 'cards.level', 'cards.url',
+                'cards.user_id', 'cards.created_at', 'cards.repeats',
+                'words.value', 'words.data'
+            );
+        $cardsUser = Paginator::create($cardsUserQuery, 10);
         return $cardsUser;
     }
-
-    
 
 }
